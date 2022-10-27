7433d57d7acc32addc18c93b25d27d6ae19f7844
codak77 <seanchi77@gmail.com>
2022-10-27 17:48:34 +0100

    functions

---
+++ functions.py
@@ -0,0 +1,36 @@
+"""
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+    return total
+
+
+print(multiply(2, 3, 4, 5))
+"""
+
+"""
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+"""
+
+def fizz_buzz(input):
+
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
+
+
+print(fizz_buzz(15))
